0612a66f79cacb23924d3f3e037e4c89e277c2ec
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-10-26 13:10:53 +0530

    Added: Functionality to return invoice for a given ID

--- InvoiceGenerator.java
+++ InvoiceGenerator.java
@@ -5,7 +5,19 @@ public class InvoiceGenerator {
 	private static final double MINIMUM_COST_PER_KILOMETER = 10;
 	private static final int COST_PER_TIME = 1;
 	private static final double MINIMUM_FARE = 5;
-
+	private RideRepository rideRepository;
+	
+	public InvoiceGenerator() {
+		rideRepository = new RideRepository();
+	}
+	
+	/**
+	 * UC 1
+	 * 
+	 * @param distance
+	 * @param time
+	 * @return
+	 */
 	public double calculateFare(double distance, int time) {
 		double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time*COST_PER_TIME;
 		return Math.max(MINIMUM_FARE, totalFare);
@@ -24,6 +36,20 @@ public class InvoiceGenerator {
 		}
 		InvoiceSummary invoiceSummary = new InvoiceSummary(rides.length, totalFare);
 		return invoiceSummary;
+	}
+	
+	/**
+	 * UC 4
+	 * 
+	 * @param userId
+	 * @param rides
+	 */
+	public void addRides(String userId, Ride[] rides) {
+		rideRepository.addRides(userId, rides);
+	}
+	
+	public InvoiceSummary getInvoiceSummary(String userId) {
+		return this.calculateFare(rideRepository.getRides(userId));
 	}
 
 }
