719ee68bb7fea2b59c778ead49c5c90026e69b42
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-10-26 08:48:48 +0530

    Welcome to Cab Invoice Generator

---
+++ InvoiceGenerator.java
@@ -0,0 +1,11 @@
+package gradleAssignment;
+
+public class InvoiceGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Cab Invoice Generator");
+
+	}
+
+}
